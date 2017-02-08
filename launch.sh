542feb23130936f4f20355e4857291d2a860946d
Jérôme Mahuet <jerome.mahuet@gmail.com>
2017-02-08 11:34:09 +0100

    bash launch script

--- launch.sh
+++ launch.sh
@@ -1,4 +1,6 @@
+#!/usr/bin/env bash
+
 SOCK_NAME="geoloc.sock"
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-cd $DIR
-GDAL_DATA=share $DIR/geolocator $DIR/$SOCK_NAME
+cd ${DIR}
+GDAL_DATA=share ${DIR}/geolocator ${DIR}/${SOCK_NAME}
